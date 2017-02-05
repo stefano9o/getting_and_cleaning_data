a71f8624b10043b32a5d51750fe1e6880e3cf315
stefano9o <stefano.galeano@gmail.com>
2017-02-05 18:52:05 +0000

    complete the script which create the total mean (subject and activity) dataframe

--- run_analysis.R
+++ run_analysis.R
@@ -1,40 +1,61 @@
 library(dplyr)
+library(reshape2)
 
-#download the file
+##download the fzip ile
 filename <- "assignment.zip"
-
 
 if(!file.exists(filename)){
   fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
   download.file(fileUrl,filename)
 }
 
+##decompress the zip file
 if (!file.exists("UCI HAR Dataset")){ 
   unzip(filename) 
 }
 
-activityLabels <- read.table("./UCI HAR Dataset/activity_labels.txt",col.names = c("code","name"),colClasses = c("factor","character"))
+##read dataframes
+#read the Activity name dataframe
+activityLabels <- read.table("./UCI HAR Dataset/activity_labels.txt",stringsAsFactors = FALSE,col.names = c("code","name"))
+#read the features name dataframe
 features <- read.table("./UCI HAR Dataset/features.txt",stringsAsFactors = FALSE,col.names = c("code","name"))
+#select only the interested columns
 features <- features[grepl(".*mean.*|.*std.*",features$name),]
-featuresSelected <- features$code
+featuresIndex <- features$code
 featuresNames <- features$name
 #clean Data Frame names
 featuresNames <- gsub("[-|/(/)]","",featuresNames)
 featuresNames <- gsub("mean","Mean",featuresNames)
 featuresNames <- gsub("std","Std",featuresNames)
 
-test.data <- read.table("./UCI HAR Dataset/test/X_test.txt")[,featuresSelected]
-test.activities <- read.table("./UCI HAR Dataset/test/y_test.txt",col.names = "code",colClasses = "factor")
-test.activities <- merge(test.activities,activityLabels)
-test.subjects <- read.table("./UCI HAR Dataset/test/subject_test.txt",col.names = "code",colClasses = "factor")
-test.df <- cbind(test.subjects$code,test.activities$name,test.data)
+#read the feature values for the test dataframe
+test.data <- read.table("./UCI HAR Dataset/test/X_test.txt")[,featuresIndex]
+#read the activities for the test dataframe
+test.activities <- read.table("./UCI HAR Dataset/test/y_test.txt",col.names = "code")
+test.activities <- factor(test.activities$code, levels = activityLabels$code, labels = activityLabels$name)
+#read the subjects for the test dataframe
+test.subjects <- read.table("./UCI HAR Dataset/test/subject_test.txt",col.names = "code")
+#create the test dataframe
+test.df <- cbind(test.subjects$code,test.activities,test.data)
 names(test.df) <- c("subject","activity",featuresNames)
 
-train.data <- read.table("./UCI HAR Dataset/train/X_train.txt")[,featuresSelected]
-train.activities <- read.table("./UCI HAR Dataset/train/y_train.txt",col.names = "code",colClasses = "factor")
-train.activities <- merge(train.activities,activityLabels)
-train.subjects <- read.table("./UCI HAR Dataset/train/subject_train.txt",col.names = "code",colClasses = "factor")
-train.df <- cbind(train.subjects$code,train.activities$name,train.data)
+#read the feature values of the train dataframe
+train.data <- read.table("./UCI HAR Dataset/train/X_train.txt")[,featuresIndex]
+#read the activities for the train dataframe
+train.activities <- read.table("./UCI HAR Dataset/train/y_train.txt",col.names = "code")
+train.activities <- factor(train.activities$code, levels = activityLabels$code, labels = activityLabels$name)
+#read the subjects for the train dataframe
+train.subjects <- read.table("./UCI HAR Dataset/train/subject_train.txt",col.names = "code")
+#create the train dataframe
+train.df <- cbind(train.subjects$code,train.activities,train.data)
 names(train.df) <- c("subject","activity",featuresNames)
 
-total <- rbind(train.df,test.df)
+##create the total dataframe by binding the test and train dataset
+total <- rbind(test.df,train.df)
+
+##Create of the dataframe that contain the average of each variable for each activity and each subject.
+totalMelted <- melt(total, id = c("subject", "activity"))
+totalMean <- dcast(totalMelted, subject + activity ~ variable, mean)
+
+##write the totalMean dataframe into a txt file
+write.table(totalMean, "tidy.txt", row.names = FALSE, quote = FALSE)
